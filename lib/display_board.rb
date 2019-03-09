37728948c0c3819c85cbcfd757e8a27e3b12dd4d
Richard Dowland <rdowland@hotmail.co.uk>
2019-03-09 19:53:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,12 +8,3 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
-def display_board(board)
-  board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
